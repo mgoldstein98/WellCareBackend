2859d1b5acd3dea2de76e1ea031cfb891e380992
KadeJ <31259667+KadeJ@users.noreply.github.com>
2018-03-26 21:39:55 -0500

    Update WellcareDatabase.sql

--- WellcareDatabase.sql
+++ WellcareDatabase.sql
@@ -3,13 +3,15 @@
  USE Wellcare;
  
  Create TABLE User(
-	UserId int,
-    Password VARCHAR(255),
+	UserId int NOT NULL,
+    Password VARCHAR(255) NOT NULL,
     FirstName VARCHAR(255),
     LastName VARCHAR(255),
-    Email VARCHAR(255),
+    Email VARCHAR(255) NOT NULL,
     Gender VARCHAR(255),
-    HomeAddress VARCHAR(255)
+    HomeAddress VARCHAR(255),
+    PRIMARYKEY(UserId)
+
 );
 
 INSERT INTO User(UserId, Password, FirstName, LastName, Email, Gender, HomeAddress)
