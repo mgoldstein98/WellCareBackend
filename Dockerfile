7ee1995c0ed363e2772f46ece5ced832b9719a1d
mgoldstein98 <31217227+mgoldstein98@users.noreply.github.com>
2018-03-29 11:54:17 -0500

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM node:latest
 
-# Install bash
-RUN apk add --update bash && rm -rf /var/cache/apk/*
-
 # Create app directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
@@ -14,5 +11,8 @@ RUN npm install --save hapi
 
 # Bundle app source
 COPY . .
+
+EXPOSE 4000
+
 CMD [ "npm", "start" ]
 
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
+
 const Hapi = require('hapi');
 
-const server = new Hapi.Server({ port: 3600, host: 'web' });
+const server = new Hapi.Server({ host: '0.0.0.0', port: 4000 });
 
 server.route({
  method: 'GET',
  path: '/',
  handler: function (request, reply) {
     return('Hello, world!');
+    // return 'hey'
  }
 });
 
@@ -16,17 +18,27 @@ server.route({
  path: '/{name}',
  handler: function (request, reply) {
     return('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    // return 'hey'
  }
 });
 
-server.start((err) => {
-if (err) {
- throw err;
- }
- console.log(`Server running at: ${server.info.uri}`);
-});
 
-process.on('unhandledRejection', (reason, p) => {
-      console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+server  
+  .start()
+  .catch(err => {
+    console.log(err);
+  })
+
+// server.start((err) => {
+// if (err) {
+//  throw err;
+//  }
+//  console.log(`Server running at: ${server.info.uri}`);
+//  //return 'hey'
+// });
+
+// process.on('unhandledRejection', (reason, p) => {
+//       console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+
       // application specific logging, throwing an error, or other logic here
-});
+// });
