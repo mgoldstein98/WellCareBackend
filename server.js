bc82eac0dd2c6845910677332d5557c505db62c4
mgoldstein98 <31217227+mgoldstein98@users.noreply.github.com>
2018-04-12 14:27:22 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const server = new Hapi.Server({ host: '0.0.0.0', port: 4000 });
 
 var mysql = require('mysql');
 
-
 var mysqlCon = mysql.createConnection({
     //host will be the name of the service from the docker-compose file.
     host     : 'database',
@@ -17,119 +16,37 @@ var mysqlCon = mysql.createConnection({
     database : 'WellcareDB'
 });
 
-// Route for Home Landing Page 
-server.route({
- method: 'GET',
- path: '/Home',
- handler: function (request, reply) {
-    return('<h1> Welcome to WellCare</h1>');
- }
-});
 
-//Route For Specific User account 
+// server.route({
+//   method: 'GET',
+//   path: '/picture',
+//   handler: {
+//       file: 'picture.jpg'
+//   }
+// });
+
+
+// Mock up minimum of 10 endpoints in postman, mixture of get and post
+// 
+
+
 server.route({
  method: 'GET',
- path: '/account/user/{name}',
+ path: '/',
  handler: function (request, reply) {
-    return('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    return('<h1> Hello World </h1>');
     // return 'hey'
  }
 });
 
-//Route For Specific Doctor account 
-server.route({
-  method: 'GET',
-  path: '/account/doc/{name}',
-  handler: function (request, reply) {
-     return('Hello, ' + encodeURIComponent(request.params.name) + '!');
-     // return 'hey'
-  }
- });
-
-server.route({
-  method: 'POST',
-  path: '/user',
-  handler: function(request, reply) {
-    return ('User Added: ' + request.payload['lName'] + ', '
-    + request.payload['fName']);
-  }
-});
-
-//Route for returning all users
-server.route({
-  method: 'GET',
-  path: '/account/user',
-  handler: function(request, h){
-
-      return new Promise(function(resolve, reject){
-
-        mysqlCon.query("SELECT * FROM User", function (error, results, fields) {
-
-          if (error) throw error;
-
-          console.log(results); 
-
-          resolve(h.response(results));
-
-        })
-    });
-  }
-});
-
-///Route for Doctor Account page #default 
-server.route({
-  method: 'GET',
-  path: '/account/doc',
-  handler: function(request, h){
-
-      return new Promise(function(resolve, reject){
-
-          resolve(h.response("Hello World"));
-
-    });
-  }
-});
-
-//Route for Logout Page 
-server.route({
-  method: 'GET',
-  path: '/wellcare/logout',
-  handler: function(request, h){
-
-      return new Promise(function(resolve, reject){
-
-          resolve(h.response("Hello World"));
-
-    });
-  }
-});
-
-//Route for scheduling an appointment 
-server.route({
-  method: 'GET',
-  path: '/wellcare/appointments',
-  handler: function(request, h){
-
-      return new Promise(function(resolve, reject){
-
-          resolve(h.response("Hello World"));
-
-    });
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/account/user/settings',
-  handler: function(request, h){
-
-      return new Promise(function(resolve, reject){
-
-          resolve(h.response("Hello World"));
-
-    });
-  }
-});
+// server.route({
+//  method: 'GET',
+//  path: '/{name}',
+//  handler: function (request, reply) {
+//     return('Hello, ' + encodeURIComponent(request.params.name) + '!');
+//     // return 'hey'
+//  }
+// });
 
 server.route({
   method: 'POST',
@@ -161,12 +78,14 @@ server.route({
   }
 });
 
+
 server.route({
   method: 'POST',
   path: '/login',
   handler: function(request, h) {
     return new Promise(function(resolve, reject) {
-      resolve(h.response("User log-in"));
+
+      resolve(h.response("User added"));
     });
   }
 });
@@ -176,6 +95,7 @@ server.route({
   path: '/changepassword',
   handler: function(request, h) {
     return new Promise(function(resolve, reject) {
+
       resolve(h.response("User added"));
     });
   }
@@ -203,8 +123,9 @@ server.route({
   }
 });
 
+
 server
   .start()
   .catch(err => {
     console.log(err);
-})
+  })
