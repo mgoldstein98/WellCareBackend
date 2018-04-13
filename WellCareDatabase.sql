5f722f48d6dda5a746d035d2d18f74cde48c21d6
mgoldstein98 <31217227+mgoldstein98@users.noreply.github.com>
2018-04-12 19:10:48 -0500

    Add files via upload

---
+++ WellCareDatabase.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS WellcareDB;
+
+CREATE DATABASE IF NOT EXISTS WellcareDB;
+
+USE WellcareDB;
+ 
+ Create TABLE IF NOT EXISTS User(
+	UserId int NOT NULL PRIMARY KEY auto_increment,
+    Password VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255) NOT NULL,
+    Gender VARCHAR(255),
+    HomeAddress VARCHAR(255)
+
+);
+
+ Create TABLE IF NOT EXISTS Doctor(
+	doc_id int NOT NULL PRIMARY KEY auto_increment,
+    Password VARCHAR(255) NOT NULL,
+    FirstName VARCHAR(255),
+    LastName VARCHAR(255),
+    Email VARCHAR(255) NOT NULL,
+    Gender VARCHAR(255),
+    OfficeAddress VARCHAR(255),
+    specialization VARCHAR(255)
+);
+
+ Create TABLE IF NOT EXISTS Appointment (
+	appointmentID int NOT NULL PRIMARY KEY,
+    doc_id int NOT NULL,
+    user_id int NOT NULL ,
+    Date date DEFAULT NULL,
+    Time time DEFAULT NULL,
+    Reason VARCHAR(255),
+    FOREIGN KEY (doc_id) REFERENCES Doctor(doc_id),
+	FOREIGN KEY (user_id) REFERENCES User(UserId)
+);
+
+
+
+-- INSERT INTO User(UserId, Password, FirstName, LastName, Email, Gender, HomeAddress)
+-- 	VALUES(User.UserId, 'abc123', 'Max', 'Gold', 'jSmith@gmail.com', 'Male', '123 Main st.');
+-- 
+-- INSERT INTO Doctor(doc_id, Password, FirstName, LastName, Email, Gender, OfficeAddress, specialization)
+-- 	VALUES(Doctor.doc_id, 'doc123', 'Doctor', 'Doctorson M.D.', 'doctor@gmail.com', 'Male', '123 Main st.', 'bellybutton');
+--     
+-- INSERT INTO Appointment(appointmentID, doc_id, user_id, Date, Time, Reason)
+-- 	VALUES(100, 1, 1, '4/12/18', '12:00', 'Bellybutton Hurting');
+    
+
+-- SELECT * from Appointment;
--- server.js
+++ server.js
@@ -29,44 +29,20 @@ server.route({
 //Route For Specific User account 
 server.route({
  method: 'GET',
- path: '/account/user/name',
-
- //returns welcome message and user's first name from Database
+ path: '/account/user/{name}',
  handler: function (request, reply) {
-  return new Promise(function(resolve, reject){
-
-    mysqlCon.query("SELECT FirstName FROM User", function (error, results, fields) {
-
-      
-
-      console.log(results); 
-
-      resolve(reply.response("Hello " + results[0].FirstName + ", welcome to WellCare!"));
-
-    })
-});
+    return('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    // return 'hey'
  }
 });
 
 //Route For Specific Doctor account 
 server.route({
   method: 'GET',
-  path: '/account/doc/name',
-
-  //returns welcome message and doctor's last name from Database
+  path: '/account/doc/{name}',
   handler: function (request, reply) {
-    return new Promise(function(resolve, reject){
-
-      mysqlCon.query("SELECT LastName FROM Doctor", function (error, results, fields) {
-  
-        
-  
-        console.log(results); 
-  
-        resolve(reply.response("Welcome back Dr. " + results[0].LastName + "!"));
-  
-      })
-  });
+     return('Hello, ' + encodeURIComponent(request.params.name) + '!');
+     // return 'hey'
   }
  });
 
@@ -83,9 +59,7 @@ server.route({
 server.route({
   method: 'GET',
   path: '/account/user',
-
-  //returns all user data
-  handler: function(request, reply){
+  handler: function(request, h){
 
       return new Promise(function(resolve, reject){
 
@@ -95,7 +69,7 @@ server.route({
 
           console.log(results); 
 
-          resolve(reply.response(results));
+          resolve(h.response(results));
 
         })
     });
@@ -106,21 +80,13 @@ server.route({
 server.route({
   method: 'GET',
   path: '/account/doc',
-  handler: function(request, reply){
+  handler: function(request, h){
 
-    //returns all data from Doctor table
-    return new Promise(function(resolve, reject){
+      return new Promise(function(resolve, reject){
 
-      mysqlCon.query("SELECT * FROM Doctor", function (error, results, fields) {
+          resolve(h.response("Hello World"));
 
-        if (error) throw error;
-
-        console.log(results); 
-
-        resolve(reply.response(results));
-
-      })
-  });
+    });
   }
 });
 
@@ -128,11 +94,11 @@ server.route({
 server.route({
   method: 'GET',
   path: '/wellcare/logout',
-  handler: function(request, reply){
+  handler: function(request, h){
 
       return new Promise(function(resolve, reject){
 
-          resolve(reply.response("Goodbye!"));
+          resolve(h.response("Hello World"));
 
     });
   }
@@ -142,40 +108,25 @@ server.route({
 server.route({
   method: 'GET',
   path: '/wellcare/appointments',
-  handler: function(request, reply){
+  handler: function(request, h){
 
       return new Promise(function(resolve, reject){
 
-        mysqlCon.query("SELECT * FROM Appointment", function (error, results, fields) {
+          resolve(h.response("Hello World"));
 
-          if (error) throw error;
-
-          console.log(results); 
-
-          resolve(reply.response(results));
-
-        })
     });
   }
 });
 
 server.route({
   method: 'GET',
-  path: '/account/user/reason',
-  handler: function(request, reply){
+  path: '/account/user/settings',
+  handler: function(request, h){
 
-      //returns user reason for appointment
       return new Promise(function(resolve, reject){
 
-        mysqlCon.query("SELECT Reason FROM Appointment", function (error, results, fields) {
+          resolve(h.response("Hello World"));
 
-          if (error) throw error;
-
-          console.log(results); 
-
-          resolve(reply.response(results[0].Reason));
-
-        })
     });
   }
 });
@@ -185,7 +136,7 @@ server.route({
   path: '/createaccount',
   handler: function(request, reply) {
 
-    const UserId = request.payload.UserId;
+    // const UserId = request.payload.UserId;
     const Password = request.payload.Password;
     const FirstName = request.payload.FirstName;
     const LastName = request.payload.LastName;
@@ -194,7 +145,7 @@ server.route({
     const HomeAddress = request.payload.HomeAddress;
 
     return new Promise(function(resolve, reject) {
-      var sql = 'INSERT INTO User (UserId, Password, FirstName, LastName, Email, Gender, HomeAddress) VALUES(' + UserId + ',' + "'" + Password + "'" + ','
+      var sql = 'INSERT INTO User (Password, FirstName, LastName, Email, Gender, HomeAddress) VALUES(' + Password + "'" + ','
       + "'" + FirstName + "'" + ',' + "'" + LastName + "'" + ',' + "'" + Email + "'" + ',' + "'" + Gender + "'" + ',' + "'" + HomeAddress + "'" + ')';
       mysqlCon.query(sql, function (err, result) {
         if (err) {
@@ -202,8 +153,8 @@ server.route({
           resolve(reply.response("404: User not added"));
         }
         else {
-        console.log(result);
-        resolve(reply.response(result));
+          // console.log(result);
+          resolve(reply.response(result));
         }
       });
     });
@@ -213,9 +164,27 @@ server.route({
 server.route({
   method: 'POST',
   path: '/login',
-  handler: function(request, h) {
+  handler: function(request, reply) {
+  
+    const UserId = request.payload.UserId;
+    const Password = request.payload.Password;
+
     return new Promise(function(resolve, reject) {
-      resolve(h.response("User log-in"));
+      var sql = "SELECT Password FROM User WHERE UserId = '" + UserId + "';";
+
+      mysqlCon.query(sql, function (err, result) {
+        if (err) {
+          throw err;
+          resolve(reply.response("404: User not added"));
+        }
+        else {
+          var pass = result[0].Password;
+          if(Password === pass)
+            resolve(reply.response('Login Success'));
+          else
+            resolve(reply.response('Login Failed'));
+        }
+      });
     });
   }
 });
